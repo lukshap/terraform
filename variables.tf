1b4589c501fa0868fd47f4b42c05c3825ec7c2b5
Pavel Luksha <pavel_luksha@epam.com>
2019-11-28 00:20:20 +0300

    cloudwatch alarms system and isntance status checks

--- variables.tf
+++ variables.tf
@@ -31,6 +31,26 @@ variable instance_alarms {
       statistic           = "Average",
       threshold           = "80",
       alarm_description   = "This metric monitors ec2 cpu utilization"
+    },
+    {
+      comparison_operator = "GreaterThanOrEqualToThreshold",
+      evaluation_periods  = "2",
+      metric_name         = "StatusCheckFailed_Instance",
+      namespace           = "AWS/EC2",
+      period              = "120",
+      statistic           = "Average",
+      threshold           = "1",
+      alarm_description   = "This metric monitors the status of the instance status check"
+    },
+    {
+      comparison_operator = "GreaterThanOrEqualToThreshold",
+      evaluation_periods  = "2",
+      metric_name         = "StatusCheckFailed_System",
+      namespace           = "AWS/EC2",
+      period              = "120",
+      statistic           = "Average",
+      threshold           = "1",
+      alarm_description   = "This metric monitors the status of the system status check"
     }
   ]
 }
