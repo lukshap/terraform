1ed96eff869c2f4f3d1773a6b15ca4d45d39b246
Pavel Luksha <pavel_luksha@epam.com>
2019-12-02 23:31:53 +0300

    add disk utilization and memory

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "alarms" {
-  source                    = "git::git@github.com:lukshap/aws_alarms_ec2.git"
+//  source                    = "git::git@github.com:lukshap/aws_alarms_ec2.git"
+  source                    = "../aws_alarms_ec2"
   instance_alarms           = var.instance_alarms
   instance_tag_name_jenkins = var.instance_tag_name_jenkins
   instance_tag_name_dev     = var.instance_tag_name_dev
--- variables.tf
+++ variables.tf
@@ -51,6 +51,30 @@ variable instance_alarms {
       statistic           = "Average",
       threshold           = "1",
       alarm_description   = "This metric monitors the status of the system status check"
+    },
+    {
+      comparison_operator = "GreaterThanOrEqualToThreshold",
+      evaluation_periods  = "2",
+      metric_name         = "MemoryUtilization",
+      namespace           = "System/Linux",
+      period              = "120",
+      statistic           = "Average",
+      threshold           = "80",
+      alarm_description   = "This metric monitors ec2 memory utilization"
+    },
+    {
+      comparison_operator = "GreaterThanOrEqualToThreshold",
+      evaluation_periods  = "1",
+      metric_name         = "DiskSpaceUtilization",
+      namespace           = "System/Linux",
+      period              = "120",
+      statistic           = "Average",
+      threshold           = "80",
+      alarm_description   = "This metric monitors ec2 disk space \"/\" partition utilization",
+      dimensions          = {
+        MountPath         = "/",
+        Filesystem        = "/dev/nvme0n1p1"
+      }
     }
   ]
 }
