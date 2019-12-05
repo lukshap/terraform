25f935a11442184d44716efcbb94408df29af407
Pavel Luksha <pavel_luksha@epam.com>
2019-12-05 21:34:34 +0300

    add region

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "ec2_alarms" {
-//  source                    = "git::git@github.com:lukshap/aws_alarms_ec2.git"
-  source                    = "../aws_alarms_ec2"
+  source                    = "git::git@github.com:lukshap/aws_alarms_ec2.git"
+//  source                    = "../aws_alarms_ec2"
   instance_alarms           = var.instance_alarms
   instance_tag_name_jenkins = var.instance_tag_name_jenkins
   instance_tag_name_dev     = var.instance_tag_name_dev
