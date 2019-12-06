033be84d3fcc0f0c73824380ed387a2fa5369b2a
Pavel Luksha <pavel_luksha@epam.com>
2019-12-06 13:50:12 +0300

    new aws_alarms_lb module

--- main.tf
+++ main.tf
@@ -16,3 +16,10 @@ module "rds_alarms" {
   rds_dev                   = var.rds_dev
   rds_sqlserver             = var.rds_sqlserver
 }
+
+module "alb_alarms" {
+  source                    = "git::git@github.com:lukshap/aws_alarms_lb.git"
+//  source                    = "../aws_alarms_lb"
+  alb_alarms                = var.alb_alarms
+  alb_name                  = var.alb_name
+}
